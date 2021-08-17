dd675e9bc2773e40c6e3ce9d7ddfdd184a38a1d4
misterjimza <you@example.com>
2021-08-17 18:03:25 +0700

    add art1-lab3

---
+++ LAB-3/article1-lab3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int sum(string x){
+    int total=0;
+    
+    for(int i=0; i<x.size(); i++){
+        total +=  x[i] - '0';
+    }
+    cout << total << endl;
+
+    int total2=0;
+    
+    while(total!=0){
+        total2= total2 + total % 10;
+        total= total / 10;
+    }
+    cout << total2 << endl;
+
+    return 0;
+}
+
+int main() {
+    string x;
+    cin >> x ;
+    // cout << sum(x);
+    sum(x);
+    
+    
+}
